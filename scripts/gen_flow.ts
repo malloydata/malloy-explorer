6c00cee7e73770772baae31aa73bdc7720f3f580
Will Scullin <scullin@meta.com>
2025-03-07 08:08:50 -0800

    Missed files

---
+++ scripts/gen_flow.ts
@@ -0,0 +1,36 @@
+import {unstable_translateTSDefToFlowDef} from 'flow-api-translator';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const outDir = path.resolve('flow');
+const inDir = path.resolve('dist/types');
+
+const files = [
+  'index.d.ts',
+  'components/MalloyPreview.d.ts',
+  'components/QueryEditor.d.ts',
+  'components/RawPreview.d.ts',
+  'contexts/QueryEditorContext.d.ts',
+];
+const directories = ['components', 'contexts'];
+
+async function go() {
+  if (fs.existsSync(outDir)) {
+    fs.rmSync(outDir, {recursive: true});
+  }
+  for (const directory of directories) {
+    fs.mkdirSync(path.resolve(outDir, directory), {recursive: true});
+  }
+  await Promise.all(
+    files.map(async file => {
+      const inFile = path.resolve(inDir, file);
+      const outFile = path.resolve(outDir, file);
+      console.log(`Generating flow types for file ${file}`);
+      const contents = fs.readFileSync(inFile, 'utf8');
+      const flow = await unstable_translateTSDefToFlowDef(contents);
+      await fs.promises.writeFile(outFile, flow);
+    })
+  );
+}
+
+go();
