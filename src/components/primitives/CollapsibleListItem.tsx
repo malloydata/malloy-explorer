b8ed993f53ca08d08389933e2f5e903eccc0f138
Natig Tofigzade <natig@meta.com>
2025-03-20 16:35:44 -0700

    Add collapsible list item primitive component
    Signed-off-by: Natig Tofigzade <natig@meta.com>

---
+++ src/components/primitives/CollapsibleListItem.tsx
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import * as React from 'react';
+import stylex from '@stylexjs/stylex';
+import {fontStyles} from './styles';
+import Icon from './Icon';
+import {textColors} from './colors.stylex';
+
+export interface CollapsibleListItemProps {
+  /**
+   * The unique key for the list item.
+   */
+  key: string;
+
+  /**
+   * The label for the list item.
+   */
+  label: string;
+
+  /**
+   * The sublabel for the list item.
+   */
+  sublabel?: string;
+
+  /**
+   * The children items to render.
+   */
+  children: React.ReactNode[];
+}
+
+export default function CollapsibleListItem({
+  key,
+  label,
+  sublabel,
+  children,
+}: CollapsibleListItemProps) {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(prev => !prev);
+  };
+
+  return (
+    <div>
+      <div key={key} {...stylex.props(styles.main)} onClick={handleToggle}>
+        {isExpanded ? (
+          <Icon name="chevronDown" color="secondary" />
+        ) : (
+          <Icon name="chevronRight" color="secondary" />
+        )}
+        <div {...stylex.props(styles.center)}>
+          <span {...stylex.props(fontStyles.body, styles.label)}>{label}</span>
+          {sublabel && (
+            <span {...stylex.props(fontStyles.supporting, styles.sublabel)}>
+              {sublabel}
+            </span>
+          )}
+        </div>
+      </div>
+      {isExpanded && (
+        <div {...stylex.props(styles.content)}>
+          {React.Children.map(children, child => child)}
+        </div>
+      )}
+    </div>
+  );
+}
+
+const styles = stylex.create({
+  main: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '8px',
+    gap: '8px',
+    borderRadius: '8px',
+    cursor: 'pointer',
+    backgroundColor: {
+      ':hover': '#DDE2E8',
+    },
+  },
+  center: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+  },
+  label: {
+    flexGrow: 1,
+    fontWeight: 700,
+  },
+  sublabel: {
+    flexGrow: 1,
+    color: textColors.secondary,
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '0px 0px 8px 32px',
+    gap: '4px',
+  },
+});
--- src/components/primitives/index.ts
+++ src/components/primitives/index.ts
@@ -10,5 +10,6 @@ export {default as TokenGroup} from './TokenGroup';
 export {default as EditableToken} from './EditableToken';
 export {default as Card} from './Card';
 export {default as TextInput} from './TextInput';
+export {default as CollapsibleListItem} from './CollapsibleListItem';
 
 export {type IconType} from './utils/icon';
