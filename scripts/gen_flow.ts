bbd5d8fb1b607c50546350c371b332ff99ae6855
Will Scullin <scullin@meta.com>
2025-04-02 11:51:46 -0700

    Updated type list (#105)
    Attempt to better capture our public interfaces.

--- scripts/gen_flow.ts
+++ scripts/gen_flow.ts
@@ -2,40 +2,37 @@ import {unstable_translateTSDefToFlowDef} from 'flow-api-translator';
 import * as fs from 'fs';
 import * as path from 'path';
 
-const outDir = path.resolve('flow');
+const outDir = path.resolve('@flowtypes');
 const inDir = path.resolve('dist/types');
 
 const files = [
   'index.d.ts',
-  'components/MalloyPreview.d.ts',
+  'components/MalloyExplorerProvider.d.ts',
   'components/QueryPanel/QueryEditor.d.ts',
   'components/QueryPanel/QueryActionBar.d.ts',
-  'components/RawPreview.d.ts',
+  'components/ResultPanel/ResultPanel.d.ts',
+  'components/ResultPanel/SubmittedQuery.d.ts',
   'components/SourcePanel/SourcePanel.d.ts',
-];
-const directories = [
-  'components',
-  'components/QueryPanel',
-  'components/SourcePanel',
+  'contexts/ExplorerPanelsContext.d.ts',
 ];
 
 async function go() {
   if (fs.existsSync(outDir)) {
     fs.rmSync(outDir, {recursive: true});
   }
-  for (const directory of directories) {
-    fs.mkdirSync(path.resolve(outDir, directory), {recursive: true});
+
+  for (const file of files) {
+    const inFile = path.resolve(inDir, file);
+    const outFile = path.resolve(outDir, file.replace('.d.ts', '.flow.js'));
+    const outFileDir = path.dirname(outFile);
+    console.log(`Generating flow types for file ${file}`);
+    const contents = fs.readFileSync(inFile, 'utf8');
+    const flow = await unstable_translateTSDefToFlowDef(contents);
+    if (!fs.existsSync(outFileDir)) {
+      fs.mkdirSync(outFileDir, {recursive: true});
+    }
+    fs.writeFileSync(outFile, flow);
   }
-  await Promise.all(
-    files.map(async file => {
-      const inFile = path.resolve(inDir, file);
-      const outFile = path.resolve(outDir, file.replace('.d.ts', '.flow.js'));
-      console.log(`Generating flow types for file ${file}`);
-      const contents = fs.readFileSync(inFile, 'utf8');
-      const flow = await unstable_translateTSDefToFlowDef(contents);
-      await fs.promises.writeFile(outFile, flow);
-    })
-  );
 }
 
 go();
