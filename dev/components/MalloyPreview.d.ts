05fa49166c431b5f9756b80cd4a1f5661e1c36e8
Will Scullin <scullin@meta.com>
2025-04-02 11:47:55 -0700

    Remove stray files

--- dev/components/MalloyPreview.d.ts
+++
@@ -1,6 +0,0 @@
-import * as Malloy from '@malloydata/malloy-interfaces';
-export interface MalloyPreviewProps {
-    source: Malloy.SourceInfo;
-    query?: Malloy.Query;
-}
-export declare function MalloyPreview({ source, query }: MalloyPreviewProps): import("react/jsx-runtime").JSX.Element | null;
