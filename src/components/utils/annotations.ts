357571959e3dad2ac8439b7d6d01b2b5218b7f2a
Andy Reding <andrewreding@meta.com>
2025-04-10 16:24:45 -0700

    fix missing annotation case
    Signed-off-by: Andy Reding <andrewreding@meta.com>

--- src/components/utils/annotations.ts
+++ src/components/utils/annotations.ts
@@ -6,8 +6,12 @@ export const getDescriptionAnnotation = (
   annotations: Array<Malloy.Annotation>
 ): string | undefined => {
   const descriptions = annotations
-    ?.filter(a => a.value.startsWith(DESCRIPTION_ANNOTATION_PREFIX))
-    ?.map(a => a.value.slice(DESCRIPTION_ANNOTATION_PREFIX.length).trim());
+    .filter(a => a.value.startsWith(DESCRIPTION_ANNOTATION_PREFIX))
+    .map(a => a.value.slice(DESCRIPTION_ANNOTATION_PREFIX.length).trim());
+
+  if (!descriptions.length) {
+    return undefined;
+  }
 
   return descriptions.join('\n');
 };
