d578b980738491107681c7ab75b558cfe653a92b
Andy Reding <andrewreding@meta.com>
2025-04-21 16:17:43 -0700

    styling
    Signed-off-by: Andy Reding <andrewreding@meta.com>

--- src/components/primitives/HoverCard.tsx
+++ src/components/primitives/HoverCard.tsx
@@ -24,6 +24,7 @@ export function HoverCard({children, customStyle}: HoverCardProps) {
 
 const styles = stylex.create({
   container: {
+    zIndex: '201',
     boxShadow:
       '0 1px 2px 0 rgba(0, 0, 0, 0.1), 0 2px 12px 0 rgba(0, 0, 0, 0.1)',
     backgroundColor: 'white',
