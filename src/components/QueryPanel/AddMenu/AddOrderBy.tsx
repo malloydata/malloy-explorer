16508378cf551e78a57731888c61c2c1762bc137
Michael Booth <michaelbooth@meta.com>
2025-05-15 17:40:45 -0400

    Fix remaining order_by issues

--- src/components/QueryPanel/AddMenu/AddOrderBy.tsx
+++ src/components/QueryPanel/AddMenu/AddOrderBy.tsx
@@ -38,7 +38,7 @@ export function AddOrderBy({rootQuery, view, search}: AddEmptyNestProps) {
           // In this context, field.name is actually the full name of the field, and
           // the segment fields also include the qualified name, therefore we can
           // provide an empty path value.
-          return segment ? !segmentHasOrderBy(segment, [], field.name) : true;
+          return segment ? !segmentHasOrderBy(segment, field.name) : true;
         }),
     [outputSchemaFields, segment]
   );
