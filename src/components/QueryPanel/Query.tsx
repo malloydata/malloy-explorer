029f77e54877d75144b7525866d889ba4805bbf6
Will Scullin <scullin@meta.com>
2025-05-14 17:23:23 -0700

    Malloy 0.0.277 (#257)
    * Malloy 0.0.277
    
    * lint

--- src/components/QueryPanel/Query.tsx
+++ src/components/QueryPanel/Query.tsx
@@ -10,7 +10,6 @@ import * as Malloy from '@malloydata/malloy-interfaces';
 import {
   ASTArrowQueryDefinition,
   ASTQuery,
-  ASTSegmentViewDefinition,
 } from '@malloydata/malloy-query-builder';
 
 import {ViewDefinition} from './ViewDefinition';
@@ -45,10 +44,6 @@ export function Query({rootQuery, query, setQuery}: QueryProps) {
     onCurrentNestQueryPanelChange?.(null);
     onCurrentNestViewChange?.(null);
   };
-
-  const canEditViz =
-    query.definition instanceof ASTArrowQueryDefinition &&
-    query.definition.view instanceof ASTSegmentViewDefinition;
 
   return (
     <div onPointerDownCapture={focusMainQueryPanel}>
@@ -109,7 +104,7 @@ export function Query({rootQuery, query, setQuery}: QueryProps) {
       >
         {query.definition instanceof ASTArrowQueryDefinition ? (
           <div style={{display: 'flex', flexDirection: 'column', gap: 8}}>
-            {!query.isEmpty() && canEditViz && (
+            {!query.isEmpty() && (
               <Visualization rootQuery={rootQuery} view={query} />
             )}
             <ViewDefinition
