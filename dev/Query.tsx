78f03dd39567501bf9586741ed142e576328b48e
Will Scullin <scullin@meta.com>
2025-08-22 08:58:28 -0700

    Better query conversion failure handling (#306)

--- dev/Query.tsx
+++ dev/Query.tsx
@@ -32,6 +32,7 @@ const App = () => {
 
   const onQueryChange = React.useCallback(
     (query: Malloy.Query | string | undefined) => {
+      setQuery(query);
       console.info('Pushing', query);
       window.history.pushState(
         query,
